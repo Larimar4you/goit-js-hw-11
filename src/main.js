4552b816d1cc7767ec01ec774d44df0be9ce2463
Larimar4you <Zajanezlaja@gmail.com>
2025-02-08 22:37:51 +0100

    git push

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import axios from 'axios';
+
+axios
+  .get('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  })
+  .catch(error => {
+    console.log(error);
+  });
