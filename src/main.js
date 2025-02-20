f9092f7e294ffe28b3ae7cdff7f8a9ad1a1b65cb
Larimar4you <Zajanezlaja@gmail.com>
2025-02-20 15:49:30 +0100

    main

--- src/main.js
+++ src/main.js
@@ -1,14 +1,49 @@
-import axios from 'axios';
+// Описаний у документаціїimport iziToast from "izitoast";
+// Додатковий імпорт стилівimport "izitoast/dist/css/iziToast.min.css";
+// npm install izitoast --save
 
-axios
-  .get('https://jsonplaceholder.typicode.com/users')
-  .then(response => {
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// Імпортуємо функціїimport { fetchImages } from "./js/pixabay-api";
+
+import { fetchImages } from './js/picabay-api';
+import { renderGallery } from './js/render-functions';
+import { showNoResultsMessage } from './js/render-functions';
+import { clearGallery } from './js/render-functions';
+
+const loader = document.querySelector('.loader');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async event => {
+  // Скасовуємо перезавантаження сторінки
+  event.preventDefault();
+
+  const searchQuery = event.target.elements.query.value.trim();
+  if (!searchQuery) {
+    return showError('Please enter your request');
+  }
+
+  clearGallery();
+  toggleLoader(true);
+
+  try {
+    const data = await fetchImages(searchQuery);
+    if (data.hits && data.hits.length) {
+      renderGallery(data.hits);
+    } else {
+      showNoResultsMessage();
+    }
+  } catch (error) {
+    console.error(error); // Виведення помилки в консоль
+    // showError("Something went wrong. Please try again!");
+  } finally {
+    toggleLoader(false);
+  }
+});
+
+function showError(message) {
+  iziToast.error({ title: 'Error', position: 'topRight', message });
+}
+
+// Функція для відображення/приховування індикатора завантаження
+function toggleLoader(isVisible) {
+  loader.style.display = isVisible ? 'block' : 'none';
+}
